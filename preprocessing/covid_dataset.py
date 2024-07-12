- Fixed bugs with normalization of intensity - Added preprocessing of the spacing of the volumes. - Added LossFunctions to the config class

preprocessing/covid_dataset.py
@@ -1,8 +1,9 @@
 import torch
+import monai
 
-
-class CovidDataset(torch.utils.data.Dataset):
+class CovidDataset(monai.data.PersistentDataset):
     def __init__(self, volumes, hrct_transform=None, cbct_transform=None):
+        super().__init__(data=volumes, transform=None, cache_dir="covid_dataset_cache")
         self.volumes = volumes
         self.hrct_transform = hrct_transform
         self.cbct_transform = cbct_transform